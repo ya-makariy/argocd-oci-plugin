62111fdbc351ecdd97b76642e732b3f0121d9519
Makarii Balashov <me@ya-makariy.com>
2024-10-06 23:08:28 +0200

    add tests

--- cmd/pull_test.go
+++ cmd/pull_test.go
@@ -1,5 +1,30 @@
 package cmd
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
+func TestMain(t *testing.T) {
+
+	t.Run("will throw an error expecting arguments", func(t *testing.T) {
+		args := []string{}
+		cmd := NewPullCommand()
+
+		c := bytes.NewBufferString("")
+		cmd.SetArgs(args)
+		cmd.SetErr(c)
+		cmd.SetOut(bytes.NewBufferString(""))
+		cmd.Execute()
+		out, err := io.ReadAll(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := "<name>{:<tag>|@<digest>} argument required to pull files"
+		if !strings.Contains(string(out), expected) {
+			t.Fatalf("expected to contain: %s but got %s", expected, out)
+		}
+	})
+}
